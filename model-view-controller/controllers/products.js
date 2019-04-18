0776deca42875e5a438a177ea81e2d87906e259a
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-17 23:02:54 -0400

    fixed bug and it now grabs products

--- model-view-controller/controllers/products.js
+++ model-view-controller/controllers/products.js
@@ -17,7 +17,7 @@ exports.postAddProduct = (req, res, next) => {
   }
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
+  Product.fetchAll(products=>{
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
@@ -26,4 +26,6 @@ exports.getProducts = (req, res, next) => {
       activeShop: true,
       productCSS: true
     });
+  });
+
 };
