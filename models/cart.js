486e4953e1495e829ce1227917c84539534cc0b6
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-20 19:57:26 -0400

    edited bug with total price in cart

--- models/cart.js
+++ models/cart.js
@@ -43,12 +43,12 @@ module.exports = class Cart {
                 return;
             }
             const updatedCart = {...JSON.parse(fileContent)};
-            const product = updatedCart.products.findIndex((prod) => prod.id === id);
+            const product = updatedCart.products.find((prod) => prod.id === id);
             const productQty = product.qty;
             updatedCart.products = updatedCart.products.filter((prod)=> prod.id !== id)
-            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+            updatedCart.totalPrice = updatedCart.totalPrice - parseInt(productPrice) * productQty;
             fs.writeFile(p, JSON.stringify(updatedCart), (err)=>{
-                console.log(err)
+                // console.log(err)
             })
         })
     }
