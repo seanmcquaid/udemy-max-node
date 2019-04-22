8c5e03e0da73898f0bf4d6c7aaa25e71f0b32435
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-22 17:35:18 -0400

    created a basic user model

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,6 +21,7 @@ exports.postAddProduct = (req, res, next) => {
     }).then((result)=>{
       // console.log(result)
       console.log("Created Product")
+      res.redirect("/admin/products")
     }).catch((err)=>{
       console.log(err)
     })
@@ -83,6 +84,15 @@ exports.getProducts = (req,res,next) =>{
 
 exports.postDeleteProduct = (req,res,next)=>{
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products")
+  Product.findByPk(prodId)
+    .then((product)=>{
+      return product.destroy()
+    })
+    .then((result)=>{
+      console.log("product deleted")
+      res.redirect("/admin/products")
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
 }
