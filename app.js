ab5ad1b4551485ddf46dd8dd037a25219d2c2e38
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-21 21:59:27 -0400

    set up DB connection

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
+const db = require("./util/database")
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute("SELECT * FROM products").then().catch();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
