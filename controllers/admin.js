7c20d9afd46fe5bb252301af5c7f3078b8d6137c
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-21 22:55:05 -0400

    added in insert query

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,7 +15,12 @@ exports.postAddProduct = (req, res, next) => {
     const description = req.body.description;
     const product = new Product(null, title, imageUrl, description, price);
     product.save()
-    res.redirect('/');
+      .then(()=>{
+        res.redirect('/');
+      })
+      .catch((err)=>{
+        console.log(err)
+      });
   }
 
 exports.getEditProduct = (req, res, next) => {
