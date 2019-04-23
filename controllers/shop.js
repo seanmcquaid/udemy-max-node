c806381e960e2d3903eb3577ea54437b3e156dda
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-22 23:18:16 -0400

    implemented post ordeR

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require("../models/product");
-const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.findAll().then((products)=>{
@@ -136,4 +135,30 @@ exports.getOrders = (req,res,next) => {
     path: "/orders",
     pageTitle : "Your Orders"
   })
+}
+
+exports.postOrder = (req,res,next)=>{
+  req.user.getCart()
+  .then((cart)=>{
+    return cart.getProducts()
+  })
+  .then((products)=>{
+    return req.user
+    .createOrder()
+    .then((order)=>{
+      return order.addProducts(products).map((product)=>{
+        product.orderItem = {quantity : product.cartItem.quantity}
+        return product
+      })
+    }).catch((err2)=>{
+      console.log(err2)
+    })
+    console.log(products)
+  })
+  .then((result)=>{
+    res.redirect("/orders")
+  })
+  .catch((err)=>{
+    console.log(err)
+  });
 }
