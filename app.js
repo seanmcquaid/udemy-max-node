7e02ced9c8edd05c4d5bb740a2b50d214fd7e713
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-24 15:25:06 -0400

    db connection complete

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,12 +29,11 @@ app.use((req,res,next)=>{
     // });
 });
 
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client)=>{
-    console.log(client)
+mongoConnect(()=>{
     app.listen(3000)
 })
--- util/database.js
+++ util/database.js
@@ -2,17 +2,27 @@ const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
-const url = "mongodb+srv://sean:trumpet15@cluster0-un7wn.mongodb.net/test?retryWrites=true"
+let _db;
+
+const url = "mongodb+srv://sean:trumpet15@cluster0-un7wn.mongodb.net/shop?retryWrites=true"
 
 const mongoConnect = (callback)=>{
     MongoClient.connect(url)
     .then((client)=>{
-        console.log("connected")
-        callback(client)
+        // console.log("connected")
+        _db = client.db();
+        callback();
     })
     .catch((err)=>{
         console.log(err)
     });
 }
 
-module.exports = mongoConnect;
+const getDb = ()=>{
+    if(_db){
+        return _db
+    }
+    throw "No database found"
+}
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
