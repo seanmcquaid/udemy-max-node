cbb27964114158365de21fef66f1c7d7ea966367
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-24 18:18:11 -0400

    added delete functionality and implemented fixes for add

--- controllers/admin.js
+++ controllers/admin.js
@@ -81,17 +81,14 @@ exports.getProducts = (req,res,next) =>{
   });
 }
 
-// exports.postDeleteProduct = (req,res,next)=>{
-//   const prodId = req.body.productId;
-//   Product.findByPk(prodId)
-//     .then((product)=>{
-//       return product.destroy()
-//     })
-//     .then((result)=>{
-//       console.log("product deleted")
-//       res.redirect("/admin/products")
-//     })
-//     .catch((err)=>{
-//       console.log(err)
-//     })
-// }
+exports.postDeleteProduct = (req,res,next)=>{
+  const prodId = req.body.productId;
+  Product.deleteById(prodId)
+    .then(()=>{
+      console.log("product deleted")
+      res.redirect("/admin/products")
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+}
--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
         this.price = price;
         this.description = description;
         this.imageUrl = imageUrl;
-        this._id = new mongodb.ObjectId(_id);
+        this._id = _id ? new mongodb.ObjectId(_id) : null;
     }
 
     save(){
@@ -55,6 +55,18 @@ class Product {
         .catch((err)=>{
             console.log(err)
         })
+    }
+
+    static deleteById(prodId){
+        const db = getDb();
+        return db.collection("products")
+        .deleteOne({_id : new mongodb.ObjectId(prodId)})
+        .then((result)=>{
+            console.log("deleted")
+        })
+        .catch((err)=>{
+            console.log(err)
+        });
     }
 }
 
