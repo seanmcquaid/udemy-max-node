578266757c3b78aa1329fe2ac73609185f5afb8f
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-25 15:04:32 -0400

    carts product page

--- controllers/shop.js
+++ controllers/shop.js
@@ -42,11 +42,8 @@ exports.getIndex = (req,res,next) => {
 }
 
 exports.getCart = (req,res,next) => {
-  req.user.getCart()
-  .then((cart)=>{
-    return cart.getProducts();
-    console.log(cart)
-  })
+  req.user
+  .getCart()
   .then((products)=>{
     res.render("shop/cart", {
       path: "/cart",
@@ -67,36 +64,6 @@ exports.postCart = (req,res,next)=>{
   }).then((result)=>{
     console.log(result)
   });
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user
-  //   .getCart()
-  //   .then((cart)=>{
-  //     fetchedCart = cart;
-  //     return cart.getProducts({where: {id: prodId}})
-  //   })
-  //   .then((products)=>{
-  //     let product;
-  //     if(products.length > 0){
-  //       product = products[0];
-  //     }
-  //     if(product){
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findByPk(prodId)
-      
-  //   })
-  //   .then((product)=>{
-  //     return fetchedCart.addProduct(product, {
-  //       through : {quantity : newQuantity} 
-  //     });
-  //   })
-  //   .then(()=>{
-  //     res.redirect("/cart")
-  //   })
-  //   .catch((err)=>{console.log(err)});
 }
 
 exports.postCartDeleteProduct = (req,res,next)=>{
--- models/user.js
+++ models/user.js
@@ -43,6 +43,26 @@ class User {
             );
     }
 
+    getCart(){
+        const db = getDb();
+        const productIds = this.cart.items.map((item)=>{
+            return item.productId
+        })
+        return db.collection("products")
+        .find({_id : {$in: productIds}})
+        .toArray()
+        .then((products)=>{
+            return products.map((product)=>{
+                return {
+                    ...product, 
+                    quantity: this.cart.items.find((item)=>{
+                    return item.productId.toString() === product._id.toString()
+                }).quantity
+            }
+            })
+        })
+    }
+
     static findById(userId){
         const db = getDb();
         return db.collection("users")
