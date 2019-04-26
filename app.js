49c6b2adafa4891ffbe10ba0c276108c439944aa
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-26 17:10:38 -0400

    starting to learn about mongoose

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 const errorController = require('./controllers/error');
-const mongoConnect = require("./util/database").mongoConnect;
 
 const User = require("./models/user")
 
@@ -36,6 +36,8 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(()=>{
-    app.listen(3000)
+mongoose.connect("mongodb+srv://sean:trumpet15@cluster0-un7wn.mongodb.net/test?retryWrites=true").then(result=>{
+    app.listen(3000);
+}).catch(err=>{
+    console.log(err)
 })
