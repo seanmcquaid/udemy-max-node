d213048645efd39c60f0e3be6458cf12658b3694
seanmcquaid <sean.mcquaid2@verizon.net>
2019-04-27 12:48:38 -0400

    first time using mongoose to create a schema and access mongo

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const mongoose = require("mongoose");
 
 const errorController = require('./controllers/error');
 
-const User = require("./models/user")
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -19,24 +17,24 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next)=>{
-    User.findById("5cc1e629a7598b55b9ccd878")
-    .then((user)=>{
-        // console.log(user.dataValues)
-        req.user = new User(user.name, user.email, user.cart, user._id);
-        // console.log(req.user)
-        next();
-    }).catch((err)=>{
-        console.log(err)
-    });
-});
+// app.use((req,res,next)=>{
+//     User.findById("5cc1e629a7598b55b9ccd878")
+//     .then((user)=>{
+//         // console.log(user.dataValues)
+//         req.user = new User(user.name, user.email, user.cart, user._id);
+//         // console.log(req.user)
+//         next();
+//     }).catch((err)=>{
+//         console.log(err)
+//     });
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect("mongodb+srv://sean:trumpet15@cluster0-un7wn.mongodb.net/test?retryWrites=true", {useNewUrlParser:true}).then(result=>{
+mongoose.connect("mongodb+srv://sean:trumpet15@cluster0-un7wn.mongodb.net/shop?retryWrites=true", {useNewUrlParser:true}).then(result=>{
     app.listen(3000);
 }).catch(err=>{
     console.log(err)
