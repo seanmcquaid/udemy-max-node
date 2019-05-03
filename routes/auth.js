956fef243833ecb27a847d16c1b840ebf1e98060
seanmcquaid <sean.mcquaid2@verizon.net>
2019-05-03 17:52:38 -0400

    started auth controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const authController = require("../controllers/auth");
 
-router.get("/login", (req,res,next)=>{
-    
-})
+router.get("/login", authController.getLogin)
 
 module.exports = router;
