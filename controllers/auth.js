4fe2170d317f4fd9100fb2be9ab632343a6e1806
seanmcquaid <sean.mcquaid2@verizon.net>
2019-05-07 13:44:45 -0400

    reviewing express sessions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,9 @@
 exports.getLogin = (req,res,next) => {
-    const isLoggedIn = req.get("Cookie").split("=")[1] === "true";
     // console.log(isLoggedIn)
     res.render("auth/login", {
       path: "/login",
       pageTitle : "Login",
-      isAuthenticated : isLoggedIn
+      isAuthenticated : false
     });
 };
 
