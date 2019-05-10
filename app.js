c2f171a7138682406731652cc2dff9bab4de6c70
seanmcquaid <sean.mcquaid2@verizon.net>
2019-05-10 15:18:28 -0400

    implemented post sign up

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require("mongoose");
-const session = require("express-session");
-const MongoDbStore = require("connect-mongodb-session")(session);
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
-const User = require("./models/user");
+const User = require('./models/user');
 
-const MONGODB_URI = "mongodb+srv://sean:trumpet15@cluster0-un7wn.mongodb.net/shop";
+const MONGODB_URI =
+  'mongodb+srv://sean:trumpet15@cluster0-un7wn.mongodb.net/shop';
 
 const app = express();
-const store = new MongoDbStore({
-    uri : MONGODB_URI,
-    collection: "sessions",
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
 });
 
 app.set('view engine', 'ejs');
@@ -26,26 +27,26 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-    secret: "my secret",
+app.use(
+  session({
+    secret: 'my secret',
     resave: false,
     saveUninitialized: false,
     store: store
-}));
+  })
+);
 
-app.use((req,res,next)=>{
-    if(!req.session.user){
-        return next();
-    }
-    User.findById(req.session.user._id)
-  .then((user)=>{
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
       req.user = user;
-      // console.log(req.user)
       next();
-  }).catch((err)=>{
-      // console.log(err)
-  });
-})
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -53,23 +54,11 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(MONGODB_URI, {useNewUrlParser:true})
-.then(result=>{
-    User.findOne().then((user)=>{
-        if(!user){
-            const user = new User({
-                name : "Sean",
-                email: "hello@gmail.com",
-                cart: {
-                    items: []
-                }
-            });
-            user.save()
-        }
-    }).catch((err)=>{
-        console.log(err)
-    })
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
     app.listen(3000);
-}).catch(err=>{
-    console.log(err)
-})
+  })
+  .catch(err => {
+    console.log(err);
+  });
