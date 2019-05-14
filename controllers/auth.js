d6d2e77d7e1a2caf633f5b9a395bc32a7ad5f403
seanmcquaid <sean.mcquaid2@verizon.net>
2019-05-14 13:44:35 -0400

    learned flash for error messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,17 +2,31 @@ const User = require('../models/user');
 const bcrypt = require("bcryptjs");
 
 exports.getLogin = (req, res, next) => {
+  // console.log(req.flash("error"))
+  let message = req.flash("error");
+  if(message.length > 0){
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: req.flash("error")
+    errorMessage: message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if(message.length > 0){
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup', 
+    errorMessage: message
   });
 };
 
@@ -52,6 +66,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
+        req.flash("error", "E-mail exists already. Please pick a different one!")
         return res.redirect('/signup');
       }
       return bcrypt.hash(password, 12)
